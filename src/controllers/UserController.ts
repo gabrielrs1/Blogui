0dc409305223e19d58487abb9797272075883dd6
Gabriell-Ribeiro <gabriellribeiro208@gmail.com>
2021-10-06 22:23:41 -0300

    User

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository"
+import * as bcrypt from 'bcrypt';
+
+class UserController {
+    async signUp(request: Request, response: Response) {
+        const { name, email, password } = request.body;
+
+        const userRepository = getCustomRepository(UserRepository);
+
+        const emailAlreadyUsed = await userRepository.findOne({ email });
+
+        if(emailAlreadyUsed) {
+            return response.status(400).json({ message: "Email already used" });
+        }
+
+        const hash = await bcrypt.hash(password, 10);
+
+        const user = userRepository.create({
+            name,
+            email,
+            password: hash
+        });
+
+        await userRepository.save(user);
+
+        return response.status(201).json(user);
+    }
+
+    async login(request: Request, response: Response) {
+        const { email, password } = request.body;
+
+        const userRepository = getCustomRepository(UserRepository);
+
+        const userAlreadyExist = await userRepository.findOne({ email });
+        
+        if(!userAlreadyExist) {
+            return response.status(404).json({ message: "Unregistered email" });
+        }
+
+        const compareUserPassword = await bcrypt.compare(password, userAlreadyExist.password);
+
+        if(!compareUserPassword) {
+            return response.status(400).json({ message: "Incorrect password" });
+        }
+
+        return response.status(200).redirect("/");
+    }
+}
+
+export { UserController }
--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,28 @@
 import { Router } from "express";
 import { CategoryController } from "./controllers/CategoryController";
 import { PostController } from "./controllers/PostController";
+import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
 const categoryController = new CategoryController();
 const postController = new PostController();
+const userController = new UserController();
 
 // Category
 router.post("/category", categoryController.create);
-router.get("/category", categoryController.show);
+router.get("/category", categoryController.showAll);
 router.put("/category", categoryController.update);
 router.delete("/category/:id", categoryController.delete);
 
 // Post
 router.post("/post", postController.create);
-router.get("/post", postController.show);
+router.get("/post", postController.showAll);
 router.put("/post", postController.update);
 router.delete("/post/:id", postController.delete);
+
+// User
+router.post("/signup", userController.signUp);
+router.post("/login", userController.login);
 
 export { router }
